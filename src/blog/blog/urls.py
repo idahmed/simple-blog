683ac6e0ac2ffd86d37e44994fd32e4e8eb0a00e
idahmed <idahmedyassine2@gmail.com>
2019-09-29 17:34:53 +0100

    fix password reset.

--- src/blog/blog/urls.py
+++ src/blog/blog/urls.py
@@ -25,6 +25,10 @@ urlpatterns = [
     path('blog/', include('main.urls')),
     path('api/', include('users.api.urls')),
 
+    #authentication
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
      #password rest
     path('password-reset/', auth_views.PasswordResetView.as_view(
             template_name='users/password_reset.html'),name='password_reset'),
--- src/blog/users/urls.py
+++ src/blog/users/urls.py
@@ -6,7 +6,5 @@ from django.contrib.auth import views as auth_views
 app_name = 'users'
 urlpatterns = [
     path('register/', views.Register.as_view(), name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-     path('logout/', auth_views.LogoutView.as_view(), name='logout'), 
-     path('profile/<int:pk>/', views.ProfileView.as_view(), name='profile'),
+    path('profile/<int:pk>/', views.ProfileView.as_view(), name='profile'),
 ]
