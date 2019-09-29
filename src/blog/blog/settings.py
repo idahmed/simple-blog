1592ec3401ac59ad6469b935245add7fdaa7c80e
idahmed <idahmedyassine2@gmail.com>
2019-09-29 17:24:29 +0100

    password reset proces

--- src/blog/blog/settings.py
+++ src/blog/blog/settings.py
@@ -149,3 +149,10 @@ LOGIN_REDIRECT_URL = 'main:home'
 LOGOUT_REDIRECT_URL = 'users:login'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'idahmedyassine2@gmail.com'
+EMAIL_HOST_PASSWORD = 'ugwjgrsztwtqzckq'
